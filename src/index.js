f268eb0331158ce23914dc1b62557f69fd2000af
Denis <logunovich@gmail.com>
2021-09-01 22:01:45 +0200

    2.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './styles.css';
-import { createStore } from './createStore';
+import { applyMiddleware, createStore } from 'redux';
+import { rootReducer } from './redux/rootReducer';
+import { increment, decrement, asyncIncrement } from './redux/actions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 const counter = document.getElementById('counter'),
       addBtn = document.getElementById('add'),
@@ -7,19 +11,27 @@ const counter = document.getElementById('counter'),
       asyncBtn = document.getElementById('async'),
       themeBtn = document.getElementById('theme');
 
-const store = createStore()
+
+const store = createStore(rootReducer, 0, applyMiddleware(thunk, logger));
 
 addBtn.addEventListener('click', () => {
-
+    store.dispatch(increment())
 });
 
 subBtn.addEventListener('click', () => {
-    
+    store.dispatch(decrement())
 });
 
 asyncBtn.addEventListener('click', () => {
- 
+    store.dispatch(asyncIncrement())
 });
+
+store.subscribe(() => {
+    const state = store.getState();
+    counter.textContent = state
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
 
 themeBtn.addEventListener('click', () => {
     // document.body.classList.toggle('dark')
